2b6d1fc114f5d1a969241f0d69985a4035ab8f10
Philippe <philippe@pcohen.fr>
2019-02-18 14:23:55 +0100

    Web Config done

--- src/fr/pcohen/controler/Home.java
+++ src/fr/pcohen/controler/Home.java
@@ -27,7 +27,7 @@ public class Home extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.getWriter().append("Vous êtes sur : ").append(request.getContextPath());
 	}
 
 	/**
